16e9810408d8660842d04a10f474cfbb69be4304
mowangshuying <1911364602@qq.com>
2026-05-12 10:10:43 +0800

    update.

--- hello-algo/4.arrayandlist/4.3mylist/main.go
+++ hello-algo/4.arrayandlist/4.3mylist/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type MyList struct {
 	arrCap int
 	arr []int
@@ -15,3 +19,86 @@ func newMyList() *MyList {
 		extendRatio:2,
 	}
 }
+
+func (this *MyList) size() int {
+	return this.arrSize
+}
+
+func (this *MyList) capacity() int {
+	return this.arrCap
+}
+
+func (this *MyList) get(index int) int {
+	if index < 0 || index >= this.arrSize {
+		panic("index out of range")
+	}
+	
+	return this.arr[index]
+}
+
+func (this *MyList) set(index int, num int) {
+	this.arr[index] = num
+}
+
+func (this *MyList) add(num int) {
+	if this.arrSize == this.arrCap {
+		this.extendCapacity()
+	}
+	
+	this.arr[this.arrSize] = num
+	this.arrSize++
+}
+
+func (this *MyList) insert(index int, num int) {
+	if index < 0 || index >= this.arrSize {
+		panic("index out of range")
+	}
+	
+	if this.arrSize == this.arrCap {
+		this.extendCapacity()
+	}
+	
+	for j := this.arrSize - 1; j >= index; j-- {
+		this.arr[j+1] = this.arr[j]
+	}
+	
+	this.arrSize++
+}
+
+func (this *MyList) remove(index int) int {
+	if index < 0 || index >= this.arrSize {
+		panic("index out of range")
+	}
+	
+	num := this.arr[index]
+	for j := index; j < this.arrSize - 1; j++ {
+		this.arr[j] = this.arr[j+1]
+	}
+	this.arrSize--
+	return num
+}
+
+
+
+func (this *MyList) extendCapacity() {
+	this.arr = append(this.arr, make([]int, this.arrCap * (this.extendRatio - 1))...)
+	this.arrCap = len(this.arr)	
+}
+
+func (this *MyList) toArray() []int {
+	return this.arr[:this.arrSize]
+}
+
+func main() {
+	mylist := newMyList()
+	mylist.add(1)
+	mylist.add(3)
+	mylist.add(5)
+	mylist.add(4)
+	mylist.add(2)
+	
+	mylist.remove(3)
+	fmt.Printf("list:\n%v", mylist)
+}
+
+
